fdf72998440fd9fc12c8d4d2eec51bef63ca97de
Iulius Uriesu <iuliusuriesu7@gmail.com>
2025-03-08 10:01:13 +0200

    Write default handler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello, world!");
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const defaultHandler = async (
+    event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            message: "Hello from the ContentFlowAI API!",
+        }),
+    };
+};
